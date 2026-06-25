07d4a7a2e88a97f63a05d9bbcad7cbf3318b5a09
athaarfatima <athaarfatima@gmail.com>
2026-06-26 04:37:29 +0500

    second commit

--- add.py
+++ add.py
@@ -1,6 +1,7 @@
 num1 = eval(input("enter first number: "))
 num2 = eval(input("enter second number: "))
+num3 = eval(input("enter third number: "))
 
-sum = num1 + num2
+sum = num1 + num2 + num3
 
 print("sum is ", sum)
